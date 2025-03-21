88f4a4c5554c65a60199b84d13b3d58ceaea08a7
Batsatsugo <corajianchristian@gmail.com>
2025-03-21 18:18:52 +0800

    print the output

--- ask_user_to_input_a_number_from_0_to_1000_print_the_number_in_6_digit_format_add_zeros_at_the_beginning_to_complete_the_6_digit.py
+++ ask_user_to_input_a_number_from_0_to_1000_print_the_number_in_6_digit_format_add_zeros_at_the_beginning_to_complete_the_6_digit.py
@@ -1,4 +1,10 @@
 # DECLARE number, formatted_number
+
 # INPUT number
+number = int(input("Please enter a number from 0 to 1000: "))
+
 # INPUT formatted_number
+formatted_number = (f'{number:06d}')
+
 # PRINT
+print(formatted_number)

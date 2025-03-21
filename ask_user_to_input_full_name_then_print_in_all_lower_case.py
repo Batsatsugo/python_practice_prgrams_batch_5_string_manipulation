8dc19b2ed8946b68619da2d31ad15b45345ce3df
Batsatsugo <corajianchristian@gmail.com>
2025-03-21 19:01:12 +0800

    print output

--- ask_user_to_input_full_name_then_print_in_all_lower_case.py
+++ ask_user_to_input_full_name_then_print_in_all_lower_case.py
@@ -1,4 +1,10 @@
 # DECLARE full_name, lower_case_name
+
 # INPUT full_name
+full_name = input("Please enter your full name: ")
+
 # INPUT lower_case_name
-# PRINT 
+lower_case_name = full_name.lower()
+
+# PRINT
+print(lower_case_name)

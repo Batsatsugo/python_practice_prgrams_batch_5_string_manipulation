33b97787bfc97c9d9ea44f9a844f9879b804239b
Batsatsugo <corajianchristian@gmail.com>
2025-03-21 19:37:30 +0800

    print output

--- ask_user_to_input_full_name_in_incorrect_casing_then_print_the_input_in_pascal_case.py
+++ ask_user_to_input_full_name_in_incorrect_casing_then_print_the_input_in_pascal_case.py
@@ -1,4 +1,9 @@
 # DECLARE full_name, pascal_case_name
+
 # INPUT full_name
+full_name = input("Please enter your full name in incorrect casing: ")
+
 # INPUT pascal_case_name
+pascal_case_name = full_name.title().replace(" ", "")
 # PRINT
+print(pascal_case_name)

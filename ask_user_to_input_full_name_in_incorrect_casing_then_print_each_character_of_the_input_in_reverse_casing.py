60e55836d17c998a4f5eb6bd30e75f81882a8b51
Batsatsugo <corajianchristian@gmail.com>
2025-03-21 19:13:53 +0800

    print output

--- ask_user_to_input_full_name_in_incorrect_casing_then_print_each_character_of_the_input_in_reverse_casing.py
+++ ask_user_to_input_full_name_in_incorrect_casing_then_print_each_character_of_the_input_in_reverse_casing.py
@@ -1,4 +1,10 @@
 # DECLARE full_name, reverse_casing_name
+
 # INPUT full_name
+full_name = input("Please enter your full name in incorrect casing: ")
+
 # INPUT reverse_casing_name
-# PRINT 
+reverse_casing_name = full_name.swapcase()
+
+# PRINT
+print(reverse_casing_name)

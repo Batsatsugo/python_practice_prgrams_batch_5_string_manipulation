3d98ab8a98c664431b864e31acaa09272618b2d4
Batsatsugo <corajianchristian@gmail.com>
2025-03-21 18:04:49 +0800

    used lstrip to remove the leading spaces in the full name

--- ask_user_to_input_fullname_with_several_space_characters_at_the_beginning_then_print_input_without_the_spaces_in_the_beginning.py
+++ ask_user_to_input_fullname_with_several_space_characters_at_the_beginning_then_print_input_without_the_spaces_in_the_beginning.py
@@ -1,4 +1,10 @@
 # DECLARE full_name, trimmed_space_in_full_name
+
 # INPUT full_name
+full_name = input("Please enter your full name with several space characters at the beginning: ")
+
 # INPUT trimmed_space_in_full_name
+trimmed_space_in_full_name = full_name.lstrip()
+
 # PRINT
+

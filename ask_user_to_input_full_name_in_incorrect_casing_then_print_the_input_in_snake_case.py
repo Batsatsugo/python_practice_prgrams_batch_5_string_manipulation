d54102f53d5d504c7748db9e59be34cd78fea387
Batsatsugo <corajianchristian@gmail.com>
2025-03-21 19:41:31 +0800

    print output

--- ask_user_to_input_full_name_in_incorrect_casing_then_print_the_input_in_snake_case.py
+++ ask_user_to_input_full_name_in_incorrect_casing_then_print_the_input_in_snake_case.py
@@ -1,4 +1,10 @@
 # DECLARE full_name, snake_case_name
+
 # INPUT full_name
+full_name = input("please enter your full name in incorrect casing: ")
+
 # INPUT snake_case_name
-# PRINT 
+snake_case_name = full_name.lower().replace(" ", "_")
+
+# PRINT
+print(snake_case_name)

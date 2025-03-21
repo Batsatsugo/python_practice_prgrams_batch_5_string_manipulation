ce972c60fb546ad28eba273d245a1846177195a4
Batsatsugo <corajianchristian@gmail.com>
2025-03-21 19:20:15 +0800

    print output

--- input_complete_statement_print_the_number_of_words_in_the_input.py
+++ input_complete_statement_print_the_number_of_words_in_the_input.py
@@ -1,4 +1,10 @@
 # DECLARE complete_statement, word_counter
+
 # INPUT complete_statement
+complete_statement = input("Please enter a complete statement: ")
+
 # INPUT word_counter
-# PRINT 
+word_counter = len(complete_statement.split())
+
+# PRINT
+print(word_counter)

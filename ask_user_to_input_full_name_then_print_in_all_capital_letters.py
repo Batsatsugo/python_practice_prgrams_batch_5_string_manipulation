77bfe2a9bd131053495852e0feefbf7e89b3f454
Batsatsugo <corajianchristian@gmail.com>
2025-03-21 18:55:17 +0800

    print

--- ask_user_to_input_full_name_then_print_in_all_capital_letters.py
+++ ask_user_to_input_full_name_then_print_in_all_capital_letters.py
@@ -1,4 +1,10 @@
 # DECLARE full_name, capital_letters_name
+
 # INPUT full_name
+full_name = input("Please enter your full name: ")
+
 # INPUT capital_letters_name
+capital_letters_name = full_name.upper()
+
 # PRINT
+print(capital_letters_name)

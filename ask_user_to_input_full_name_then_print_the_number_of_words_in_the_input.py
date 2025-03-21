fe3dce07d99387ec54b22e9d02d8da7c39a4f8e8
Batsatsugo <corajianchristian@gmail.com>
2025-03-21 19:31:34 +0800

    print output

--- ask_user_to_input_full_name_then_print_the_number_of_words_in_the_input.py
+++ ask_user_to_input_full_name_then_print_the_number_of_words_in_the_input.py
@@ -1,4 +1,10 @@
 # DECLARE full_name, character_counter
+
 # INPUT full_name
+full_name = input("Please input your full name: ")
+
 # INPUT character_counter
-# PRINT 
+character_counter = len(full_name)
+
+# PRINT
+print(character_counter)
